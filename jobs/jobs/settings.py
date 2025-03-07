38129f201c7341e448d189131f5996a2fa305d61
stevepurpose <stevenpurpose@gmail.com>
2025-03-07 22:10:55 +0100

    experimenting allowed hosts

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('MY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 REST_FRAMEWORK = {
