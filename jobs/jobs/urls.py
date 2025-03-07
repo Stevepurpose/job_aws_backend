f67e11f14b2d66918868b5d1f8afb9c81111a436
stevepurpose <stevenpurpose@gmail.com>
2025-03-07 22:01:57 +0100

    csrf_exempt

--- jobs/jobs/urls.py
+++ jobs/jobs/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 from companies.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/register/', CreateUserView.as_view(), name = "register"),
-    path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
+    path('api/user/register/', CreateUserView.as_view(), name="register"),
+    path('api/token/', csrf_exempt(TokenObtainPairView.as_view()), name="get_token"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh"),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('companies.urls')),
