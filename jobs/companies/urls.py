6d260c513bcc0befe47d8513182a3ae74b512978
stevepurpose <stevenpurpose@gmail.com>
2025-03-08 05:16:01 +0100

    added csrf exempt to some paths

--- jobs/companies/urls.py
+++ jobs/companies/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from companies import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-
 urlpatterns = format_suffix_patterns([
     path('companies/',
-        views.CompanyList.as_view(),
-        name='company-list'),
+         csrf_exempt(views.CompanyList.as_view()),
+         name='company-list'),
     path('companies/<int:pk>/',
-        views.CompanyDetail.as_view(),
-        name='company-detail'),      
-       
+         csrf_exempt(views.CompanyDetail.as_view()),
+         name='company-detail'),
 ])
