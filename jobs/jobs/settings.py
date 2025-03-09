15f2c220cc1092998a621df6de700c7a628b6e7b
stevepurpose <stevenpurpose@gmail.com>
2025-03-09 05:16:14 +0100

    added url to allowed origins

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173',]
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173', "https://myjobdiary.onrender.com",]
 
 
 CORS_ALLOW_METHODS = [
